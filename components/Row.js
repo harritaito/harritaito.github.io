61d3e9c8e4c807ac313b3b879bb24fbe89092806
Harri <halonen.harri.t@student.uta.fi>
2018-03-27 18:14:43 +0300

    Revert "sgfsg"
    This reverts commit 7794612261e1f6a0d9809e5081434c0744babd8c, reversing
    changes made to d9e8184e6a184a2921681e1789a492457bfeff9c.

---
+++ components/Row.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Row extends Component {
+
+  static propTypes = {
+    content: PropTypes.object.isRequired,
+    className: PropTypes.string
+  }
+
+  static defaultProps = {
+    content: {},
+    className: ""
+  }
+
+  render () {
+
+    return (
+      <div className={this.props.className + " row"}>
+        {this.props.content}
+      </div>
+    )
+  }
+
+}
+
+export default Row;
