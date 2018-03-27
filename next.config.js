076654dfe99cafdc9158181083aceeac7c2b2f63
Harri <halonen.harri.t@student.uta.fi>
2018-03-27 21:11:13 +0300

    Test it out

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const debug = process.env.NODE_ENV !== "production";
 // This file is not going through babel transformation.
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
@@ -12,6 +13,21 @@ module.exports = {
       '/aikakone': { page: '/aikakone' },
       '/404': { page: '/404' }
     }
+  },
+  //assetPrefix: '',
+  assetPrefix: !debug ? '/harritaito.com/' : '',
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    // Important: return the modified config
+    return config
   }
   /*,
   webpackDevMiddleware: (config) => {
