05ce2b10283c66624e6e63acb5fd6b20d8a9b4a3
Harri Halonen <harri@harritaito.com>
2025-07-29 13:35:44 +0300

    Update personal info and bio on homepage

--- pages/Home.js
+++ pages/Home.js
@@ -1,105 +1,201 @@
-import React, { Component } from 'react';
-import Link from 'next/link';
-import Project from '../components/Project';
-import Row from '../components/Row';
-import Misc from '../components/Misc';
-import Footer from '../components/Footer';
+import React, { Component } from "react";
+import Link from "next/link";
+import Project from "../components/Project";
+import Row from "../components/Row";
+import Misc from "../components/Misc";
+import Footer from "../components/Footer";
 
-import languagerobot from '../static/media/home/languagerobot.jpg';
-import kivakaupunki from '../static/media/home/kivakaupunki.jpg';
-import aikakone from '../static/media/home/aikakone.jpg';
+import languagerobot from "../static/media/home/languagerobot.jpg";
+import kivakaupunki from "../static/media/home/kivakaupunki.jpg";
+import aikakone from "../static/media/home/aikakone.jpg";
 
 class Home extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
     const date = new Date();
 
     let hours = date.getHours();
 
-    if(hours < 12){
+    if (hours < 12) {
       this.state = {
-        greeting: "Good morning"
+        greeting: "Good morning",
       };
-    }
-    else if (hours >= 12 && hours<= 17){
+    } else if (hours >= 12 && hours <= 17) {
       this.state = {
-        greeting: "Good afternoon"
+        greeting: "Good afternoon",
       };
-    }
-    else if (hours > 17 && hours <= 24){
+    } else if (hours > 17 && hours <= 24) {
       this.state = {
-        greeting: "Good evening"
+        greeting: "Good evening",
       };
-    }
-    else {
+    } else {
       this.state = {
-        greeting: "Hello"
+        greeting: "Hello",
       };
     }
   }
 
   render() {
-
     return (
       <div className="Home container">
-        <div className="pohja">
-        </div>
+        <div className="pohja"></div>
         <div className="intro">
+          <Row
+            content={
+              <h3 className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+                {this.state.greeting}, I’m
+              </h3>
+            }
+          />
 
-          <Row content={
-            <h3 className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">{this.state.greeting}, I’m</h3>
-          }/>
+          <Row
+            content={
+              <h1 className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 name">
+                Harri Halonen
+              </h1>
+            }
+          />
 
-          <Row content={
-            <h1 className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 name">Harri Halonen</h1>
-          }/>
-
-          <Row content={
-            <div className="col-xs-12 col-sm-8 col-md-9 col-lg-6 col-xl-6">
-              <div>
-                Graduating experience designer passionate about creating better customer insight, increasing business value and improving customer experience for every client. 
-                I believe in quick results withdrawn from user centered - hands on research and co-creation. 
+          <Row
+            content={
+              <div className="col-xs-12 col-sm-8 col-md-9 col-lg-6 col-xl-6">
+                <div>
+                  Experience designer passionate about creating better customer
+                  insight, increasing business value and improving customer
+                  experience for every client. I believe in quick results
+                  withdrawn from user centered - hands on research and
+                  co-creation.
+                </div>
+                <div style={{ marginTop: "1.5em" }}>
+                  You could say I'm on a quest to make technology delightful,
+                  intuitive, and accessible to everyone.
+                </div>
+                <div style={{ marginTop: "1.5em" }}>
+                  Scroll 👇 to see some highlighted projects.
+                </div>
+                <div style={{ marginTop: "1.5em" }}>
+                  Hope you enjoy this porfolio made with React and Next.js with
+                  JSX styles, it is my first crack at them and originally
+                  designed and made in 2018.
+                </div>
+                <div style={{ marginTop: "1.5em", marginBottom: "1.5em" }}>
+                  Want to learn about the person behind the work?{" "}
+                  <Link href="/about" as={"/about"} legacyBehavior>
+                    <a>
+                      Get to know me.
+                      <div
+                        style={{
+                          display: "block",
+                          outline: "none",
+                          height: 8,
+                          width: "98%",
+                          background: "rgba(139, 200, 246, 0.565)",
+                          marginTop: -9,
+                          marginLeft: 2,
+                        }}
+                      />
+                    </a>
+                  </Link>
+                </div>
               </div>
-              <div style={{marginTop: "1.5em"}}>
-                You could say I'm on a quest to make technology delightful, intuitive, and accessible to
-                everyone.
-              </div>
-              <div style={{marginTop: "1.5em"}}>
-                Currently I'm working on my Master's thesis "Industrial Interaction Heuristics/Design Principles for MR" for Tampere University's HCI program and searching for opportunities starting Spring/Summer 2020 and beyond.
-              </div>
-              <div style={{marginTop: "1.5em"}}>
-                Scroll 👇 to see some highlighted projects, or get an overall sense by <a href={'/static/HarriHalonen_Resume.pdf'} target="_blank">reading my resume.<div style={{display: 'block', outline:'none', height: 8, width: '98%', background: 'rgba(139, 200, 246, 0.565)', marginTop: -9, marginLeft: 2}} /></a> 
-              </div>
-              <div style={{marginTop: "1.5em"}}>
-                Hope you enjoy this porfolio made with React and Next.js with JSX styles, it is my first crack at them and originally designed and made in 2018.
-              </div>
-              <div style={{marginTop: "1.5em", marginBottom: "1.5em"}}>
-                Want to learn about the person behind the work? <Link href='/about' as={'/about'} legacyBehavior><a>Get to know me.<div style={{display: 'block', outline:'none', height: 8, width: '98%', background: 'rgba(139, 200, 246, 0.565)', marginTop: -9, marginLeft: 2}} /></a></Link>
-              </div>
-            </div>
-          }/>
+            }
+          />
         </div>
-        <Project title="HRI study" description="Contextual inquiry study with interviews, affinity diagrams to figure out the interaction and bit of usability with a teaching assistant robot." image={languagerobot} link="/hri-study" alt="Application for city reporting" color="red" percentage='8%'/>
-        <Project title="Kiva Kaupunki" description="From thoughts to action. Information of surroundings to city officials as well as for the public, so they can be even better." image={kivakaupunki} link="/kivakaupunki" alt="Application for city reporting" color="blue" percentage='8%'/>
-        <Project title="Aikakone" description="Dementia/Alzheimer diseases increasing and growing concern about quality of health care systems. Nurses don’t have sufficient time to engage individually and it’s difficult to get patients excited in common activities." image={aikakone} link="/aikakone" alt="Concept service for people with memory deseaces" color="purple" percentage='28%'/>
-        <Row content={
-          <h3 className={"col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 other-stuff"}>Some other stuff I’ve done</h3>
-        }/>
-        <Misc/>
-        <Row content={
-          <h3 className={"col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 get-in-touch"}>Get in touch</h3>
-        }/>
-        <Row content={
-          <div style={{marginTop: "1.5em"}} className={"col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 col-xl-4 col-xl-offset-4 contact"}>
-            Feel free to <a href="mailto:harri@harritaito.com">email me<div style={{display: 'block', height: 8, width: '98%', background: 'rgba(139, 200, 246, 0.565)', marginTop: -9, marginLeft: 2}} /></a> or <a href={"https://calendly.com/harritaito/45min/"}>book time on my calendar<div style={{display: 'block', height: 8, width: '98%', background: 'rgba(139, 200, 246, 0.565)', marginTop: -9, marginLeft: 2}} /></a>.
-          </div>
-        }/>
-        <Footer/>
+        <Project
+          title="HRI study"
+          description="Contextual inquiry study with interviews, affinity diagrams to figure out the interaction and bit of usability with a teaching assistant robot."
+          image={languagerobot}
+          link="/hri-study"
+          alt="Application for city reporting"
+          color="red"
+          percentage="8%"
+        />
+        <Project
+          title="Kiva Kaupunki"
+          description="From thoughts to action. Information of surroundings to city officials as well as for the public, so they can be even better."
+          image={kivakaupunki}
+          link="/kivakaupunki"
+          alt="Application for city reporting"
+          color="blue"
+          percentage="8%"
+        />
+        <Project
+          title="Aikakone"
+          description="Dementia/Alzheimer diseases increasing and growing concern about quality of health care systems. Nurses don’t have sufficient time to engage individually and it’s difficult to get patients excited in common activities."
+          image={aikakone}
+          link="/aikakone"
+          alt="Concept service for people with memory deseaces"
+          color="purple"
+          percentage="28%"
+        />
+        <Row
+          content={
+            <h3
+              className={
+                "col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 other-stuff"
+              }
+            >
+              Some other stuff I’ve done
+            </h3>
+          }
+        />
+        <Misc />
+        <Row
+          content={
+            <h3
+              className={
+                "col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 get-in-touch"
+              }
+            >
+              Get in touch
+            </h3>
+          }
+        />
+        <Row
+          content={
+            <div
+              style={{ marginTop: "1.5em" }}
+              className={
+                "col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 col-xl-4 col-xl-offset-4 contact"
+              }
+            >
+              Feel free to{" "}
+              <a href="mailto:harri@harritaito.com">
+                email me
+                <div
+                  style={{
+                    display: "block",
+                    height: 8,
+                    width: "98%",
+                    background: "rgba(139, 200, 246, 0.565)",
+                    marginTop: -9,
+                    marginLeft: 2,
+                  }}
+                />
+              </a>{" "}
+              or{" "}
+              <a href={"https://calendly.com/harritaito/45min/"}>
+                book time on my calendar
+                <div
+                  style={{
+                    display: "block",
+                    height: 8,
+                    width: "98%",
+                    background: "rgba(139, 200, 246, 0.565)",
+                    marginTop: -9,
+                    marginLeft: 2,
+                  }}
+                />
+              </a>
+              .
+            </div>
+          }
+        />
+        <Footer />
         <style jsx>{`
           .Home h1 {
-            margin-top: -.5rem;
+            margin-top: -0.5rem;
             font-family: Trirong, serif;
             margin-bottom: 1.2rem;
           }
@@ -109,7 +205,9 @@ class Home extends Component {
           }
 
           @media only screen and (max-width: 45rem) {
-            .Home {padding-top: 2rem;}
+            .Home {
+              padding-top: 2rem;
+            }
           }
 
           .pohja {
@@ -118,19 +216,21 @@ class Home extends Component {
             left: 0;
             height: 100%;
             width: 100%;
-            background: url('../static/media/pohja.svg');
+            background: url("../static/media/pohja.svg");
             background-size: cover;
             z-index: -1;
-            opacity: .55;
+            opacity: 0.55;
           }
-          
+
           .Home .other-stuff {
             text-align: center;
             margin-top: -1.5em;
             margin-bottom: 1.5em;
           }
           @media only screen and (max-width: 45rem) {
-            .Home .other-stuff {margin-top: -3.5em;}
+            .Home .other-stuff {
+              margin-top: -3.5em;
+            }
           }
 
           .Home .get-in-touch {
@@ -140,25 +240,29 @@ class Home extends Component {
           }
 
           @media only screen and (max-width: 45rem) {
-            .Home .get-in-touch {margin-top: 2em;}
+            .Home .get-in-touch {
+              margin-top: 2em;
+            }
           }
 
           .Home .contact {
             text-align: center;
           }
 
-          @media only screen and (max-width: 412px)  {
+          @media only screen and (max-width: 412px) {
             .Home .name {
               font-size: 2.8em;
               line-height: 3rem;
             }
           }
           @media only screen and (max-width: 45rem) {
-            .Home name {font-size: 1.8em;}
+            .Home name {
+              font-size: 1.8em;
+            }
           }
-          .ReactModal__Content--after-open{
-            marginTop: 5vh;
-            maxWidth: 89vh;
+          .ReactModal__Content--after-open {
+            margintop: 5vh;
+            maxwidth: 89vh;
           }
         `}</style>
       </div>
--- pages/about.js
+++ pages/about.js
@@ -1,56 +1,103 @@
-import React, { Component } from 'react';
-import Row from '../components/Row';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
+import React, { Component } from "react";
+import Row from "../components/Row";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 class About extends Component {
-
   render() {
-
     return (
       <div className="About container">
-
-        <Navbar nextProjectLink={null} nextProjectName={null} color={"grey"}/>
+        <Navbar nextProjectLink={null} nextProjectName={null} color={"grey"} />
 
         <div className={"content"}>
-
-          <Row content={
-            <div className="my-photo-container col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-              <div className={"my-photo"}>
-                <img src="../static/media/about/me.jpg" alt={"Me"}/>
+          <Row
+            content={
+              <div className="my-photo-container col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                <div className={"my-photo"}>
+                  <img src="../static/media/about/me.jpg" alt={"Me"} />
+                </div>
               </div>
-            </div>
-          }/>
+            }
+          />
 
-          <Row content={
-            <h3 className="col-xs-12 col-sm-12 col-md-12 col-lg-offset-3 col-lg-6 col-xl-6">Hey there!</h3>
-          }/>
+          <Row
+            content={
+              <h3 className="col-xs-12 col-sm-12 col-md-12 col-lg-offset-3 col-lg-6 col-xl-6">
+                Hey there!
+              </h3>
+            }
+          />
 
-          <Row content={
-            <p className="about-me-copy col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6 col-xl-6">
-              Hi, I'm glad you’d like to get more familiar! Here’s me in quick bulletpoints:
-            </p>
-          }/>
+          <Row
+            content={
+              <p className="about-me-copy col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6 col-xl-6">
+                Hi, I'm glad you’d like to get more familiar! Here’s me in quick
+                bulletpoints:
+              </p>
+            }
+          />
 
-          <Row content={
-            <ul className="about-me-copy col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6 col-xl-6 list">
-              <li>From Tampere, Finland, where I volunteer for <a href={'https://tribetampere.com/'} target="_blank">Tribe Tampere<div style={{display: 'block', outline:'none', height: 8, width: '98%', background: 'rgba(139, 200, 246, 0.565)', marginTop: -9, marginLeft: 2}} /></a><span className="emoji" role="img" aria-label="smile">😊</span></li>
-              <li>I brew craft beer <span className="emoji" role="img" aria-label="clinking beer glasses">🍻</span>and love parings of wine and food <span role="img" aria-label="wine glasss">🍷</span></li>
-              <li>Non-fiction audiobook and podcast addict and help on <a href={'https://www.spreaker.com/user/tribetampere'} target="_blank">Tribecast TRE<div style={{display: 'block', outline:'none', height: 8, width: '98%', background: 'rgba(139, 200, 246, 0.565)', marginTop: -9, marginLeft: 2}} /></a> <span className="emoji" role="img" aria-label="headphones">🎧</span></li>
-              <li>I love film and capturing a moment myself <span className="emoji" role="img" aria-label="film">📽</span></li>
-              <li>Lauri Markkanen and Chicago Bulls fan. I play for <a href={'https://www.akateeminenmahti.fi/koripallo/'} target="_blank">Akateeminen Mahti<div style={{display: 'block', outline:'none', height: 8, width: '98%', background: 'rgba(139, 200, 246, 0.565)', marginTop: -9, marginLeft: 2}} /></a> <span className="emoji" role="img" aria-label="basketball">🏀</span></li>
-            </ul>
-          }/>
-          
-          <Row content={
-            <p className="about-me-copy col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6 col-xl-6">
-              Thanks for visiting, it truly means a lot. The page does set at cookie for the media and user experience. If you have any suggestions hit up the "webmaster" (me) at harri [at] harritaito [dot] com . <span className="emoji" role="img" aria-label="vulcan salute">🖖</span>
-            </p>
-          }/>
+          <Row
+            content={
+              <ul className="about-me-copy col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6 col-xl-6 list">
+                <li>
+                  From Tampere, Finlan.{" "}
+                  <span className="emoji" role="img" aria-label="smile">
+                    😊
+                  </span>
+                </li>
 
+                <div style={{ marginTop: "1.5em" }}>
+                  Check out my 2021 Master's thesis called{" "}
+                  <a
+                    href={"https://urn.fi/URN:NBN:fi:tuni-202107046179"}
+                    target="_blank"
+                  >
+                    Interaction Design Principles for Industrial XR
+                    <div
+                      style={{
+                        display: "block",
+                        outline: "none",
+                        height: 8,
+                        width: "98%",
+                        background: "rgba(139, 200, 246, 0.565)",
+                        marginTop: -9,
+                        marginLeft: 2,
+                      }}
+                    />
+                  </a>{" "}
+                  for Tampere University's HCI program.
+                </div>
+                <li>
+                  I love film and capturing a moment myself{" "}
+                  <span className="emoji" role="img" aria-label="film">
+                    📽
+                  </span>
+                </li>
+                <li>
+                  Still active hooper{" "}
+                  <span className="emoji" role="img" aria-label="basketball">
+                    🏀
+                  </span>
+                </li>
+              </ul>
+            }
+          />
+
+          <Row
+            content={
+              <p className="about-me-copy col-xs-12 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6 col-xl-6">
+                Thanks for visiting, it truly means a lot. The page does set at
+                cookie for the media and user experience.
+                <span className="emoji" role="img" aria-label="vulcan salute">
+                  🖖
+                </span>
+              </p>
+            }
+          />
         </div>
 
-        <Footer/>
+        <Footer />
         <style jsx>{`{
           .About .content {
             padding-top: 1rem;
