09fe98cf5c590393736ebc48565e594ae4941734
H̷a̵r̶r̴i̵ <harri@harritaito.com>
2025-07-29 14:00:25 +0300

    Fix image zoom styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import Fonts from "../components/Fonts";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "react-medium-image-zoom/dist/styles.css";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
