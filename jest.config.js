003305967298e23ab564afcbee5625da679a6a5f
Harri Halonen <harri@harritaito.com>
2026-05-23 02:20:29 +0300

    build: re-enable Next SWC

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   // Use the basic Node environment so tests run without needing jsdom
   testEnvironment: 'node',
-  setupFiles: ['./test-setup.js']
+  setupFiles: ['./test-setup.js'],
+  transform: {
+    '^.+\\.[jt]sx?$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  moduleNameMapper: {
+    '\\.(svg|png|jpg|jpeg|gif|webp)$': '<rootDir>/test-file-stub.js'
+  }
 };
